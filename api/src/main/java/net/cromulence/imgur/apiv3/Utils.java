9fad4b4ab2d60addf83bf0a703a2aa8362460341
cromulence <cromulence@users.noreply.github.com>
2017-09-04 13:21:26 +0100

    fixing date formatiing bug

--- api/src/main/java/net/cromulence/imgur/apiv3/Utils.java
+++ api/src/main/java/net/cromulence/imgur/apiv3/Utils.java
@@ -4,43 +4,12 @@ import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_TIME;
 
 import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 
 public class Utils {
-
-    private static final long SECOND = 1000;
-    private static final long MINUTE = 60 * SECOND;
-    private static final long HOUR = 60 * MINUTE;
-
-    private static final DateTimeFormatter DTF = new DateTimeFormatterBuilder()
-                .parseCaseInsensitive()
-                .append(ISO_LOCAL_DATE)
-                .appendLiteral(' ')
-                .append(ISO_LOCAL_TIME).toFormatter();
-
-    private Utils() {}
-
-    public static String formatHHMM(long interval) {
-        long milli = interval % Utils.SECOND;
-
-        long sec = (interval - milli) % Utils.MINUTE;
-
-        long min = (interval - (milli + sec)) % Utils.HOUR;
-
-        long hour = (interval - (milli + sec + min));
-
-        return String.format("%dh%02dm", (hour / Utils.HOUR), (min / Utils.MINUTE));
-    }
-
-    public static synchronized String formatUnixTime(long datetime) {
-        Instant instant = Instant.ofEpochSecond(datetime);
-        return DTF.format(instant);
-    }
-
-    public static String formatUnixTime(String datetime) {
-        return formatUnixTime(Long.parseLong(datetime));
-    }
 
     public static String asCommaSeparatedList(String[] strings) {
 
